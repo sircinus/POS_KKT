d7a18e31c9b413b5b8e997055519254659ff75b2
Romeo Kurniawan <135410975+sircinus@users.noreply.github.com>
2025-07-04 10:46:48 +0700

    Update mailer.php

--- src/functions/mailer.php
+++ src/functions/mailer.php
@@ -11,13 +11,6 @@ $mail = new PHPMailer(true);
 $mail->isSMTP();
 $mail->SMTPAuth = true;
 
-$mail->Host = 'smtp-relay.brevo.com';
-$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-$mail->Port = 587;
-
-$mail->Username = '88b424001@smtp-brevo.com';
-$mail->Password = 'TamdAbfUOqPEw19C';
-
 $mail->isHTML(true);
 
 return $mail;
